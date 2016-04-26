475e69a6cc4b67888b0d062ab31ce1d5eb0c78f5
LeeOlivia <1961310878@qq.com>
2016-04-26 14:23:58 +0800

    about post

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 var express=require('express');
 var app=express();
+var bodyParser=require('body-parser');
+
+var urlencodedParser=bodyParser.urlencoded({extended:false})
+
 
 app.get('/index.html',function(req,res){
 res.sendFile( __dirname + "/" + "index.html" );	
 });
-app.get('/process_get', function (req, res) {
+
+app.post('/process_post',urlencodedParser,function (req, res) {
 response = {
-first_name:req.query.first_name,
-last_name:req.query.last_name
+first_name:req.body.first_name,
+last_name:req.body.last_name
 };
 console.log(response);
 res.end(JSON.stringify(response));
